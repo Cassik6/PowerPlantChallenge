f800956b6998a2cf9e21aede17241a6f4dd5560a
Opap's Ditudidi <Opaps.Ditudidi@satellit.be>
2021-10-11 09:29:46 +0200

    Adding  Logs

--- PowerPlantChallenge/Controllers/ProductionPlanController.cs
+++ PowerPlantChallenge/Controllers/ProductionPlanController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using PowerPlantChallenge.Mapping;
 using PowerPlantChallenge.Models;
 using PowerPlantChallenge.Models.DTOs;
 using PowerPlantChallenge.Services;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PowerPlantChallenge.Controllers
 {
@@ -11,19 +14,36 @@ namespace PowerPlantChallenge.Controllers
     public class ProductionPlanController : ControllerBase
     {
         private readonly ICostEfficiencyCalculationService costEfficiencyCalculationService;
+        private readonly ILogger<ProductionPlanController> logger;
 
-        public ProductionPlanController(ICostEfficiencyCalculationService costEfficiencyCalculationService)
+        public ProductionPlanController(ICostEfficiencyCalculationService costEfficiencyCalculationService, ILogger<ProductionPlanController> logger)
         {
             this.costEfficiencyCalculationService = costEfficiencyCalculationService;
+            this.logger = logger;
         }
 
         [Route("/productionplan")]
         [HttpPost]
         public ActionResult<List<PowerProduction>> ProductionPlan([FromBody] PayloadDTO dtoPayload)
         {
-            var payload = DTOToPayLoad.Map(dtoPayload);
+            try
+            {
+                var payload = DTOToPayLoad.Map(dtoPayload);
+            
+                return costEfficiencyCalculationService.CalculatePowerProduction(payload.Powerplants, payload.NeededLoad);
+            }
+            catch (ValidationException exception)
+            {
+                logger.LogError(exception, exception.Message, exception.ValidationResult);
+                return BadRequest(exception.Message);
+               
+            }
+            catch(Exception exception)
+            {
+                logger.LogError(exception, exception.Message);
+                return StatusCode(501);
+            }
 
-            return costEfficiencyCalculationService.CalculatePowerProduction(payload.Powerplants, payload.NeededLoad);
         }
     }
 }

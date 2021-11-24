74aa203362071b895d277e10be67bf5e67451402
Opap's Ditudidi <Opaps.Ditudidi@satellit.be>
2021-11-24 16:21:05 +0100

    refactoring

--- PowerPlantChallenge/Controllers/ProductionPlanController.cs
+++ PowerPlantChallenge/Controllers/ProductionPlanController.cs
@@ -45,5 +45,32 @@ namespace PowerPlantChallenge.Controllers
             }
 
         }
+        //[Route("/productionplan/ws")]
+        //[HttpPost]
+        //public async Task Post()
+        //{
+        //    if (HttpContext.WebSockets.IsWebSocketRequest)
+        //    {
+        //        using WebSocket webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+        //        await Echo(HttpContext, webSocket);
+        //    }
+        //    else
+        //    {
+        //        HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        //    }
+        //}
+
+        //private async Task Echo(HttpContext context, WebSocket webSocket)
+        //{
+        //    var buffer = new byte[1024 * 4];
+        //    WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        //    while (!result.CloseStatus.HasValue)
+        //    {
+        //        await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+        //        result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        //    }
+        //    await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        //}
     }
 }
